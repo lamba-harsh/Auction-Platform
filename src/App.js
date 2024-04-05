4eca0af0e8b9b5bed91d519f6e165f570af489b9
somyagupta752 <128929533+somyagupta752@users.noreply.github.com>
2024-04-05 19:40:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Signin from './components/Signin'
 import Navbar from './components/Navbar'
 import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Auction from './components/Auction';
+import Bidders from './components/Bidders';
+
 function App() {
   return (
     <div className="App">
@@ -14,6 +16,7 @@ function App() {
       <Route path="/signin" element={<Signin />} />
       <Route path="/home" element={<Navbar />} />
       <Route path="/auction" element={[<Navbar />,<Auction />]} />
+      <Route path="/bidders" element={[<Navbar />,<Bidders />]} />
       </Routes>
       </Router> 
 
