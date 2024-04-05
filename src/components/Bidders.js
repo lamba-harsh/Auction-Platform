7ff2742163130a623ee560e6be96839c1eda30e9
somyagupta752 <128929533+somyagupta752@users.noreply.github.com>
2024-04-05 19:37:48 +0530

    Update and rename Bidder.js to Bidders.js

---
+++ src/components/Bidders.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Bidder.css'
+import Menu from './Menu';
+import { useState } from 'react'
+import items from './data';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+const Bidders = () => {
+  const [menuItems, setMenuItems] = useState(items)
+return(
+  <main>
+  <section className='menu section'>
+    <div className='title'>
+      <h2>our menu</h2>
+      <div className='underline'></div>
+    </div>
+    <Menu items={menuItems} />
+  </section>
+</main>
+)
+    }
+export default Bidders;
